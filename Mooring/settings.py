002b6f8f0d5fa06aabfc51a29d1bee7f7e3101e1
Risoxas <davidag93@gmail.com>
2023-03-30 18:27:31 -0600

    serving css

--- Mooring/settings.py
+++ Mooring/settings.py
@@ -31,9 +31,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = os.path.join(BASE_DIR, 'static')
-
 
 # Application definition
 
@@ -128,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
