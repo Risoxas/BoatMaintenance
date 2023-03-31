b38476fa0e3fd756832b56e3e1469a6384ce2eca
Risoxas <davidag93@gmail.com>
2023-03-30 18:33:43 -0600

    add mimetypes for html

--- Mooring/settings.py
+++ Mooring/settings.py
@@ -17,6 +17,7 @@ import os
 
 
 mimetypes.add_type("text/css", ".css", True)
+mimetypes.add_type("text/html", ".html", True)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
