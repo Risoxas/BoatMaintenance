f9552ab29fa7e199567a388e606f27ead5b19c2a
Risoxas <davidag93@gmail.com>
2023-04-04 20:51:05 -0600

    imagen y fix de campos

--- boats/admin.py
+++ boats/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.models import Group
 
 from .models import Boats
+
 # Register your models here.
 
 admin.site.register(Boats)
--- boats/models.py
+++ boats/models.py
@@ -6,8 +6,12 @@ from django.db import models
 class Boats(models.Model):
     type = models.CharField(max_length=200)
     name = models.CharField(max_length=200)
-    mooring_no = models.IntegerField()
+    mooring_no = models.CharField(max_length=200)
     mooring_location = models.CharField(max_length=200)
     depth = models.IntegerField()
-    # customer_id = models.ForeignKey()
-    last_check = models.DateTimeField()
+    # customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    last_check = models.DateField()
+    image = models.ImageField(null=True)
+
+# class Customer(models.Model):
+#     boat = models.ForeignKey(Boats)
