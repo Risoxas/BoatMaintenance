77fd65e84579624d177f715cbb16796bb29ffffc
Risoxas <davidag93@gmail.com>
2023-04-09 14:00:36 -0600

    change static

--- Mooring/settings.py
+++ Mooring/settings.py
@@ -136,7 +136,7 @@ MEDIA_URL = '/media/'
 # MEDIA_ROOT = BASE_DIR/'media'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/media/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'media', 'static'),)
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'media', 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'media', 'staticfiles')
 
 # Default primary key field type
