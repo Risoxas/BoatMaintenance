755d398054889ef79ca49d9ccda3f008ea9e0757
Risoxas <davidag93@gmail.com>
2023-04-09 13:53:01 -0600

    fix folder structure

--- Mooring/urls.py
+++ Mooring/urls.py
@@ -25,6 +25,5 @@ urlpatterns = [
             {'document_root': settings.MEDIA_ROOT}, name='media'),
 ]
 
-if (settings.DEBUG):
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
